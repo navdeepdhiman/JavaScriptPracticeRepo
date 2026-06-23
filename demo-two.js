5c9766a3cabbfbf60d2d64d012047fabf87d345b
Navdeep Dhiman <navdeepdhiman91@outlook.com>
2026-06-23 11:10:51 +0530

    Updated practice files

--- demo-two.js
+++ demo-two.js
@@ -1,3 +1,5 @@
 console.log("navdeep");
 console.log("navdeep");
-console.log("navdee",9,"dhiman");
+console.log("navdee",9,"dhiman"); //semicolon is optional in js but it is good practice to use it. It helps to avoid errors and makes the code more readable.
+process.stdout.write("navdeep"); //it is used to print the output without a new line at the end. It is useful when we want to print multiple values in the same line.
+process.stdout.write("dhiman"); //it is used to print the output without a new line at the end. It is useful when we want to print multiple values in the same line.
